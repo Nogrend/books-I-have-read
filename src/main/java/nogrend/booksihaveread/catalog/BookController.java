b981b2b4046ed5da3e4c05ec14b2458d4687e2ff
bram <nogrend@msn.com>
2025-04-08 22:02:22 +0200

    add overview of books

--- src/main/java/nogrend/booksihaveread/catalog/BookController.java
+++ src/main/java/nogrend/booksihaveread/catalog/BookController.java
@@ -3,14 +3,28 @@ package nogrend.booksihaveread.catalog;
 import nogrend.booksihaveread.catalog.domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
-    @GetMapping("/books")
-    public String books(Model model) {
-        model.addAttribute("model", Book.of("test", "123-456-7890"));
+    @GetMapping("/overview")
+    public String bookOverview(Model model) {
+        List<Book> books = List.of(
+                Book.of("Clean Code", "9780132350884"),
+                Book.of("The Pragmatic Programmer", "9780135957059"),
+                Book.of("Head First Design Patterns", "9780596007126")
+        );
+        model.addAttribute("books", books);
         return "homePage";
+    }
+
+    @PostMapping("/books")
+    @ResponseBody
+    public Book addBook(@RequestBody Book book) {
+        return book;
     }
 }
